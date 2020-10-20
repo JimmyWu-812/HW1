1839dbc724490c6c812f41552b99032d6499fb09
JimmyWu-812 <59006023+JimmyWu-812@users.noreply.github.com>
2020-10-20 23:26:30 +0800

    10/20

--- p1.py
+++ p1.py
@@ -1,20 +1,27 @@
 from scipy.sparse import *
 import numpy as np
 
-
 def p1_has_cycle(sets):
-    # TODO
-    # return True if the graph has cycle; return False if not
-    '''
-      HINT: You can `print(sets)` to show what the matrix looks like
-        If we have a directed graph with 2->3 4->1 3->5 5->2 0->1
-               0  1  2  3  4  5
-            0  0  0 -1  1  0  0
-            1  0  1  0  0 -1  0
-            2  0  0  0 -1  0  1
-            3  0  0  1  0  0 -1
-            4 -1  1  0  0  0  0
-        The size of the matrix is (5,6)
-    '''
-    return False
-
+  m=csr_matrix(sets)
+  n=m.shape[0]
+  #print(m.todense())
+  while True:
+    #print(m.shape[0])
+    try:
+      #csr_matrix
+      res=hstack([[[1]]*(n-1),identity(n-1)])*m
+      #print(res.todense())
+      res2=csr_matrix(res.power(2).sum(axis=1))
+      if res2.count_nonzero() != res2.shape[0]:
+        return True
+      else:
+        c1=find(res.T[find(m[0]==-1)[1][0]]==-1)[1]
+        c2=find(res2.T==2)[1]
+        toBeAppended = res[np.intersect1d(c1,c2),:]
+        m=vstack([m,toBeAppended])
+        #print(m.todense())
+    except:
+      return False
+    m=m[1:]
+    n=m.shape[0]
+    #print('-'*40)
--- p2.py
+++ p2.py
@@ -1,21 +1,17 @@
 from scipy.sparse import *
 import numpy as np
 
-
 def p2_has_cycle(sets):
-    # TODO
-    # return True if the graph has cycle; return False if not
-    '''
-      HINT: You can `print(sets)` to show what the matrix looks like
-        If we have a directed graph with 2->3 4->1 3->5 5->2 0->1
-               0  1  2  3  4  5
-            0  0  1  0  0  0  0
-            1  0  0  0  0  0  0
-            2  0  0  0  1  0  0
-            3  0  0  0  0  0  1
-            4  0  1  0  0  0  0
-            5  0  0  1  0  0  0
-        The size of the matrix is (6,6)
-    '''
+    m=csr_matrix(sets)
+    #print(m.todense())
+    original=m.copy()
+    n=m.shape[0]
+    for i in range(n):
+        diag=csr_matrix(m.diagonal())
+        #print(diag)
+        #print(diag.count_nonzero())
+        if diag.count_nonzero() != 0:
+            return True
+        m=original*m
+        #print(m.todense())
     return False
-
